7edffde8020146483217ec4b3b35387803ee9c27
samantadearaujo@gmail.com <samantadearaujo@gmail.com>
2020-08-18 20:21:37 +0100

    change: seeds  users replace password_hash

--- exams/priv/repo/seeds/002_seeds_users.exs
+++ exams/priv/repo/seeds/002_seeds_users.exs
@@ -3,10 +3,17 @@ alias Exams.Accounts.User
 
 Repo.insert! %User{
   username: "Adm",
-  password: "123",
+  password_hash: "$argon2id$v=19$m=131072,t=8,p=4$/c/91ll/Hmilfe2m1KZBYw$wwjN6LfnXxt4t2mqZv8Ew/E4W7FYsanRpCin0ekf1Nc",
   name: "Administrador",
   profile: 1
   }
 
+  Repo.insert! %User{
+    username: "Teacher_Master",
+    password: "$argon2id$v=19$m=131072,t=8,p=4$i/pfS/9KT4V6L37RkwLk8g$gut6vgklxVgAbkzdphWz9OTCnhD8B4VK16o+BhO2EXs",
+    name: "Teacher Master",
+    profile: 2
+    }
+  
 
 
