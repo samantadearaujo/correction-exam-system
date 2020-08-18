f55d13dd3deed7f7f2cf3add426a0e26c518415e
samantadearaujo@gmail.com <samantadearaujo@gmail.com>
2020-08-18 20:20:44 +0100

    change:  add field virtual to password and validation password and put Argon hash

--- exams/lib/exams/accounts/user.ex
+++ exams/lib/exams/accounts/user.ex
@@ -4,7 +4,8 @@ defmodule Exams.Accounts.User do
 
   schema "users" do
     field :name, :string
-    field :password, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
     field :profile, :integer
     field :username, :string
     #has_one :profile, Exams.Accounts.UserProfile
@@ -17,5 +18,12 @@ defmodule Exams.Accounts.User do
     user
     |> cast(attrs, [:name, :username, :password, :profile])
     |> validate_required([:name, :username, :password, :profile])
+    |> validate_length(:password, min: 6)
+    |> put_pass_hash()
   end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
 end
