1ba5c616a36fcb02fcf273dca051ef7752b0e84a
Samanta Araújo <samantaaraujo@Samantas-MacBook-Air.local>
2021-03-27 23:12:14 -0300

    config

--- exams/config/prod.exs
+++ exams/config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :exams, ExamsWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "specific-hairy-manta.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
